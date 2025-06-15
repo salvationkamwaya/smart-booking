6c82aaa282567771d607beb98c81cbb44a392dc6
salvationkamwaya <salvationkamwaya77@gmail.com>
2025-06-14 22:57:27 -0700

    provider dashboard ok

--- src/main/java/com/smartappointments/booking_system/Controller/ClientController.java
+++ src/main/java/com/smartappointments/booking_system/Controller/ClientController.java
@@ -1,4 +1,22 @@
 package com.smartappointments.booking_system.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/client")
 public class ClientController {
+
+    @GetMapping
+    public String client(){
+        return "client/client-panel";
+    }
+
+
+
+
+
 }
+
--- src/main/java/com/smartappointments/booking_system/Controller/ProviderController.java
+++ src/main/java/com/smartappointments/booking_system/Controller/ProviderController.java
@@ -1,4 +1,20 @@
 package com.smartappointments.booking_system.Controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/provider")
 public class ProviderController {
+    @GetMapping
+    public String index(){
+      return "provider/provider-panel";
+
+
+    }
+
+
+
 }
